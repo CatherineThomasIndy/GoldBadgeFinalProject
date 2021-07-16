960a0cde9849dc6c8eb3758cc45b4b12ec7a0973
Catie Thomas <catherine.thomas.indy@gmail.com>
2021-07-16 19:29:43 -0400

    scaffolded challenge two, decided to do that instead of six

---
+++ ChallengeTwoApp/ProgramUI.cs
@@ -0,0 +1,51 @@
+﻿using ChallengeTwoApp.Consoles;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChallengeTwoApp
+{
+    public class ProgramUI
+    {
+        private bool _isRunning = true;
+        private ICustomConsole _con;
+        public ProgramUI(ICustomConsole console)
+        {
+            _con = console;
+        }
+
+        public void Run()
+        {
+            RunMenu();
+        }
+
+        private void RunMenu()
+        {
+            while (_isRunning)
+            {
+                string userInput = GetMenuSelection();
+                OpenMenuItem(userInput);
+            }
+        }
+
+        private string GetMenuSelection()
+        {
+            _con.Clear();
+            _con.PrintMenu();
+            string userInput = _con.ReadLine();
+            return userInput;
+        }
+
+        private void OpenMenuItem(string userInput)
+        {
+            _con.Clear();
+
+            switch (userInput)
+            {
+
+            }
+        }
+    }
+}
---
+++ ChallengeTwoTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ChallengeTwoTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ChallengeTwoTests")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("6e5b3183-9b89-4468-bbb3-d27d5c2e9d57")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
